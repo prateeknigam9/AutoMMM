0646d95790bb21ab7a3e454fd77236f0597ab20c
prateeknigam <prateeknigam9@gmail.com>
2025-06-16 17:38:27 +0100

    updated proposal report

---
+++ autommm/src/excel_updater.py
@@ -0,0 +1,7 @@
+
+import sys
+import os
+
+# ---- TEMP FIX FOR MODULE IMPORTS ----
+# Adds project root (3 levels up from this file) to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
