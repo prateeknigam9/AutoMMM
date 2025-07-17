4d213c9fcb52fc25f6735aa924c586cab5ba63fb
Prateek Nigam <prateeknigam9@gmail.com>
2025-07-17 23:32:05 +0100

    clear repo to reset

--- memory/memory_store.py
+++
@@ -1,3 +0,0 @@
-from langchain.memory import ConversationBufferMemory
-
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
--- utils/colors.py
+++
@@ -1,9 +0,0 @@
-# utils/colors.py
-from colorama import Fore, Style, init
-init(autoreset=True)
-
-def system_message(text: str) -> str:
-    return f"{Fore.GREEN}{text}{Style.RESET_ALL}"
-
-def system_input(text: str) -> str:
-    return f"{Fore.YELLOW}{text}{Style.RESET_ALL}"
